9336e7bd9610b1d72165dcdf0a06865a33d7c544
Albert Leao <albertleao@live.com>
2013-04-05 02:11:47 -0400

    asset changes

--- application/views/locations/main.php
+++ application/views/locations/main.php
@@ -8,8 +8,26 @@
 
   <a href="/locations/milton">
     Milton, Georgia<br />
-    12650 Crabapple Rd<br />
-    678-624-1090
+
+    <div class="location_description">
+      12650 Crabapple Rd<br />
+      678-624-1090<br /><br />
+
+      Sunday/Monday<br />
+      11 am - 11 pm<br /><br />
+      
+      Tuesday - Thursday<br />
+      11 am - Midnight<br />
+
+      Friday - Saturday<br />
+      11 am - 2 am<br /><br />
+
+      Express Lunch (M-F)<br />
+      11 am - 3 pm<br /><br />
+
+      Sunday Brunch<br />
+      11 am - 3 pm
+    </div>
   </a>
 
 </div>
@@ -23,9 +41,23 @@
 
   <a href="/locations/brookhaven">
     Brookhaven, Georgia<br />
-    705 Town Boulevard, Suite Q380<br />
-    404-816-5739
 
+    <div class="location_description">
+      705 Town Boulevard, Suite Q380<br />
+      404-816-5739<br /><br />
+
+      Sunday - Wednesday<br />
+      11 am - Midnight<br /><br />
+      
+      Thursday - Saturday<br />
+      11 am - 2 am<br /><br />
+
+      Express Lunch (M-F)<br />
+      11 am - 3 pm<br /><br />
+      
+      Sunday Brunch<br />
+      11 am - 3 pm 
+    </div>
   </a>
 
 </div>
