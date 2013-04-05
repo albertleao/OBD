171bc79ed3a57dcfa209c4d87154c2c7261df281
Albert Leao <albertleao@live.com>
2013-04-05 12:51:10 -0400

    hide footer

--- application/views/layouts/pagefooter.php
+++ application/views/layouts/pagefooter.php
@@ -1,11 +1,11 @@
 <div class="clear"></div>
-
+<!--
 <div id="pagefooter">
 	<ul id="footer_navigation">
 		<li><a href="/application">Application for hire</a></li>
 	</ul>
 </div>
-
+-->
 
 </div><!-- end of container 12 -->
 
