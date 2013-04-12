696d6a3aacf99d45a1f2991f15a4b0fb0b157efd
Albert Leao <albertleao@live.com>
2013-04-12 00:52:18 -0400

    events

--- application/views/events/main.php
+++ application/views/events/main.php
@@ -1,7 +1,85 @@
 
-<div class="grid_6" style="float: left; font-family: madfont; font-size: 22px;color: gold; text-align: center;">
+<div class="grid_12">
 
-	St. Practice day. April 17th.
+	<div class="event">
+	
+		St. Practice day. April 17th.
+
+		<p>
+			At Olde Blind Dog we never stop practicing for St. Patrick’s Day, because the best way to acquire the perfect celebration skills is to repeat them over and over. So join us on the 17th of every 	month for great Irish fun (we're giving you 11 chances to get this right). All you need to participate is an official Countdown Tour T-Shirt (ask your server for pricing and details). Then wear 	your t-shirt every time the calendar hits 17 and enjoy great drink specials, free giveaways and more.
+		</p>
+
+		<p>
+			<b>Every 17th:</b> $4 Pints: Guinness/Harp/Smithwicks/Kilkenny
+		</p>
+
+		<p>
+			<b>April 17th:</b> Free Guinness T-Shirt
+		</p>
+
+	</div>
+
+	<hr>
+
+	<div class="event">
+	
+		Publix Aprons Cooking School – April 18
+
+		<p>
+			Register now and join Olde Blind Dog’s Adam Legrand and Trevor O’Dowd at 7 p.m. on April 18 at Publix Aprons Cooking School in Alpharetta (4305 State Bridge Rd).
+		</p>
+
+		<p>
+				If you’ve wondered what goes into our upscale pub food, this is your chance to grab a front-row seat and see our chefs work their Celtic magic. The menu for this event includes:
+			<br />
+				--Scotch Eggs (paired with Harp lager)
+			<br />
+				--Guinness Stout Onion Soup (paired with Guinness)
+			<br />
+				--Shepherd’s Pie (paired with Cabernet)
+			<br />
+				--Sticky Toffee Pudding (paired with Irish Coffee)
+		</p>
+
+	</div>
+
+	<hr>
+
+	<div class="event">
+	
+		Bourbon Tasting – May 2
+
+		<p>
+			Reserve your spot now for our next Bourbon Tasting, from 7 p.m. to 9 p.m. on May 2 at our Milton location. During this enjoyable event, we will feature Dickel Rye and Dickel Barrel Select as well as Bulleit Rye and Bulleit 10 Year.
+		</p>
+
+		<p>
+			The cost is $25 per person and includes light appetizers.
+		</p>
+
+		<p>
+			<b>George Dickel Rye:</b> Is 95 percent rye and 5 percent malted barley that is distilled and then aged in new charred oak barrels for at least five years. It carries aromas of toasty oak, spicy rye, citrus, pineapple, caramel, cinnamon, tangy cherry and menthol. The palate shows the aromas in a mild evolution of flavors that is initially sweet, with hints of fruit and builds to sapid oak nuance with vanilla, spice and caramel leading all the way through. The finish gently dawdles while quietly revealing roasted nuts, fennel, sassafras and camphor.
+		</p>
+
+		<p>
+			<b>George Dickel Barrel Select:</b> Master Distiller John Lunn selects approximately 10 barrels of his best, most mellow whisky to produce an exceptionally fine small-batch Tennessee whisky. Aged 10–12 years, this 86-proof gem provides a consistently smooth taste, perfect balance, rich amber color and a hint of vanilla. Aided by charcoal mellowing and the slow aging process, these distinct characteristics create a super-premium whisky in a class of its own.
+		</p>
+
+		<p>
+			<b>Bulleit Rye:</b> Aged in white oak barrels that are charred to the maximum before being filled, this whiskey is made with 95 percent rye, 5 percent malted barley and is a copper color with slight green patina. It’s brisk, vibrant, and loaded with spice (crisp mint, warming cinnamon, cocoa, hint of anise). Fruit -- ripe nectarine, apricot, golden raisin -- drizzled with maple syrup. Dry oak rounds out the finish. Bold enough for cocktails. Mature enough to drink neat (or on the rocks).
+		</p>
+
+		<p>
+			<b>Bulleit 10 Year:</b> Choice Bulleit Bourbon barrels to age in charred American white oak barrels, resulting in a richer more complex bourbon experience. It’s russet in color, rich with oaky aromas and hints of vanilla and dried fruit with a long smoky finish.
+		</p>
+
+		<p>
+			Seating is limited so call us at 678-624-1090 to make your reservation.
+		</p>
+	</div>
+
+
+
 
 </div>
 
