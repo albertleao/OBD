86a89562b4448e9ff118d0ca7fced9a96ed06261
Albert Leao <albertleao@live.com>
2013-05-18 11:03:57 -0400

    added craft beer event without logos

--- application/views/events/main.php
+++ application/views/events/main.php
@@ -3,67 +3,38 @@
 
 	<div class="event">
 	
-		Send-off Party and Fundraiser for Megan Hill – April 25	
-
+			Join us for the 2013 Craft Beer Festival and 5K Road Race
+		
 		<p>
-			Join us on the patio April 25 as we host a proper Irish send-off (and hopefully raise a few bucks)  for U.S. Olympic Bobsledder Megan Hill. Festivities begin at 5:30 p.m.
+			Olde Blind Dog is excited to announce its sponsorship of the 2013 Craft Beer Festival and 5K Road Race in downtown Alpharetta on June 22, 2013. The festival will take place from 6 p.m. - 11:30 p.m. with beer, live music and entertainment with the race beginning at 8 p.m.
 		</p>
 
 		<p>
-			Megan (from Woodstock, Ga.) is a former gymnast and track and field athlete at Auburn. She is training full-time to keep her spot as a driver on the women’s U.S. National Bobsled team and to compete in the 2014 and 2018 Winter Olympics.
+			<b>Craft Beer Festival</b><br />
+			Over 100 craft beer vendors from around the U.S. will line the streets of downtown Alpharetta for the huge street party which will begin 6 p.m. with live music.. Entry to the festival is $10 and will get you a souvenir glass that can be used during the event. Stroll the streets and participate in a craft brewing seminar by Blind Murphy’s Craft Beer Market. Olde Blind Dog will provide its typically delicious food.
 		</p>
 
 		<p>
-			<b>George Dickel Rye:</b> Is 95 percent rye and 5 percent malted barley that is distilled and then aged in new charred oak barrels for at least five years. It carries aromas of toasty oak, spicy rye, citrus, pineapple, caramel, cinnamon, tangy cherry and menthol. The palate shows the aromas in a mild evolution of flavors that is initially sweet, with hints of fruit and builds to sapid oak nuance with vanilla, spice and caramel leading all the way through. The finish gently dawdles while quietly revealing roasted nuts, fennel, sassafras and camphor.
+			<b>5K Road Race</b><br />
+			The 5K Road Race will begin at 8 p.m. at the Corner Deli. Registration for the race is $30 before June 14 and $35 after June 14 and the day of the race from 6- 7:30 p.m. All runners will receive a race t-shirt and a free beer from Sierra Nevada to celebrate their finish. After being cheered on by festival-goers upon crossing the finish line, stay for the after party which will continue until 11:30 p.m. with live music and entertainment.
 		</p>
 
 		<p>
-			To help Megan raise money for training expenses, we will raffle off a $100 Olde Blind Dog gift card.  Buy a raffle ticket and get $4 draft pints and 20 percent off appetizers during the send-off party
+			<b>Schedule of Events</b><br />
+			6 - 7:30 p.m.- 5K Race Day Registration<br />
+			6 p.m. - Craft Beer Festival and packet pickup begins<br />
+			8 p.m.- Race Starts at the Corner Deli in Alpharetta (Start and finish just off Milton Ave and Main St in downtown Alpharetta, next to the Corner Deli.)<br />
+			Post-Race After Party – Stay after to enjoy the Craft Beer Festival, live music and entertainment!<br />
+			Every 5K participant of legal age will receive one free beer!
 		</p>
 
 		<p>
-			Megan will be on hand to sign autographs, take pictures and talk to you about her bobsledding experiences. See you on the patio April 25!  
+			For more information about the event <a href="www.awesomealpharetta.com">www.awesomealpharetta.com</a>.
 		</p>
 
 	</div>
 
 	<hr>
-
-	<div class="event">
-	
-		Bourbon Tasting – May 2
-
-		<p>
-			Reserve your spot now for our next Bourbon Tasting, from 7 p.m. to 9 p.m. on May 2 at our Milton location. During this enjoyable event, we will feature Dickel Rye and Dickel Barrel Select as well as Bulleit Rye and Bulleit 10 Year.
-		</p>
-
-		<p>
-			The cost is $25 per person and includes light appetizers.
-		</p>
-
-		<p>
-			<b>George Dickel Rye:</b> Is 95 percent rye and 5 percent malted barley that is distilled and then aged in new charred oak barrels for at least five years. It carries aromas of toasty oak, spicy rye, citrus, pineapple, caramel, cinnamon, tangy cherry and menthol. The palate shows the aromas in a mild evolution of flavors that is initially sweet, with hints of fruit and builds to sapid oak nuance with vanilla, spice and caramel leading all the way through. The finish gently dawdles while quietly revealing roasted nuts, fennel, sassafras and camphor.
-		</p>
-
-		<p>
-			<b>George Dickel Barrel Select:</b> Master Distiller John Lunn selects approximately 10 barrels of his best, most mellow whisky to produce an exceptionally fine small-batch Tennessee whisky. Aged 10–12 years, this 86-proof gem provides a consistently smooth taste, perfect balance, rich amber color and a hint of vanilla. Aided by charcoal mellowing and the slow aging process, these distinct characteristics create a super-premium whisky in a class of its own.
-		</p>
-
-		<p>
-			<b>Bulleit Rye:</b> Aged in white oak barrels that are charred to the maximum before being filled, this whiskey is made with 95 percent rye, 5 percent malted barley and is a copper color with slight green patina. It’s brisk, vibrant, and loaded with spice (crisp mint, warming cinnamon, cocoa, hint of anise). Fruit -- ripe nectarine, apricot, golden raisin -- drizzled with maple syrup. Dry oak rounds out the finish. Bold enough for cocktails. Mature enough to drink neat (or on the rocks).
-		</p>
-
-		<p>
-			<b>Bulleit 10 Year:</b> Choice Bulleit Bourbon barrels to age in charred American white oak barrels, resulting in a richer more complex bourbon experience. It’s russet in color, rich with oaky aromas and hints of vanilla and dried fruit with a long smoky finish.
-		</p>
-
-		<p>
-			Seating is limited so call us at 678-624-1090 to make your reservation.
-		</p>
-	</div>
-
-</div>
-
 
 
 </div>
