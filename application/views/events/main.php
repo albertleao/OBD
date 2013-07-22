7a7e919ea1099ed5d79822c12695dc22fad179b9
Albert Leao <albertleao@live.com>
2013-07-22 10:40:11 -0400

    events page

--- application/views/events/main.php
+++ application/views/events/main.php
@@ -1,17 +1,50 @@
-<div class="grid_12">
+<div class="grid_6">
+
+	<h1>
+		Milton, GA Events
+	</h1>
+
+	<hr>
 
 	<? foreach($events->result() as $event) { ?>
-	
-		<div class="event">
-			
-			<?= $event->description; ?>
+		
+		<? if($event->location == "milton"){ ?>
 
-		</div>
+			<div class="event">
+				
+				<?= $event->description; ?>
 
-		<hr>
+			</div>
+
+			<hr>
+		<? } ?>
 
     <? } ?>
 
+</div>
+
+<div class="grid_6">
+	
+	<h1>
+		Brookhaven, GA Events
+	</h1>
+
+	<hr>
+
+	<? foreach($events->result() as $event) { ?>
+
+		<? if($event->location == "brookhaven"){ ?>
+
+			<div class="event">
+				
+				<?= $event->description; ?>
+
+			</div>
+
+			<hr>
+		<? } ?>
+
+    <? } ?>
 
 </div>
 
